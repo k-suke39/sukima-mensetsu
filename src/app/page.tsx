99ab9a61393b8a0101168f78040d6fed4189ca8b
k-suke39 <pz.mozarla@gmail.com>
2023-11-14 17:06:11 +0900

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,191 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { useState, useEffect } from 'react';
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+import { Button } from '@/components/ui/button';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { Progress } from '@/components/ui/progress';
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default function CardWithForm() {
+	const questions = [
+		{ q: '自己紹介をお願いします', time: 30 },
+		{
+			q: '前職で頑張ったことや成果等の実績はありますか？',
+			time: 60,
+		},
+		{
+			q: '前職で1番大変だったことや失敗した経験はありますか？',
+			time: 60,
+		},
+		{
+			q: '働く上で大切にしていたこと等ありますか？',
+			time: 60,
+		},
+		{
+			q: '1社目を選んだ理由を教えてください。',
+			time: 45,
+		},
+		{
+			q: 'エンジニアを目指したきっかけを教えてください。',
+			time: 60,
+		},
+		{
+			q: '志望理由を教えてください。',
+			time: 60,
+		},
+		{
+			q: '現在勉強中の言語や技術はありますか？',
+			time: 45,
+		},
+		{
+			q: '弊社のサービスの感想や改善点は何かありますか?',
+			time: 45,
+		},
+		{
+			q: 'キャリアプランを教えてください。',
+			time: 60,
+		},
+		{
+			q: '強みを教えてください。',
+			time: 30,
+		},
+		{
+			q: '自己PRをお願いします。',
+			time: 30,
+		},
+		{
+			q: '弱みを教えてください。',
+			time: 30,
+		},
+		{
+			q: '趣味を教えてください。',
+			time: 30,
+		},
+		{
+			q: '学習はどのように進めていますか？',
+			time: 30,
+		},
+		{
+			q: '就活はどのような軸で企業を探ししていますか？',
+			time: 60,
+		},
+		{
+			q: '退職理由を教えてください。',
+			time: 60,
+		},
+		{
+			q: '希望の年収額を教えてください。',
+			time: 30,
+		},
+		{
+			q: '学習中に意識していることを教えてください。',
+			time: 30,
+		},
+		{
+			q: 'チーム開発の経験はありますか？',
+			time: 30,
+		},
+		{
+			q: '何かご質問はありますか？（逆質問）',
+			time: 60,
+		},
+		{
+			q: '作成したWebアプリについて説明してください。',
+			time: 45,
+		},
+		{ q: 'どのくらいの期間で実装しましたか？', time: 30 },
+		{
+			q: '使用技術について説明してください。',
+			time: 60,
+		},
+		{
+			q: 'インフラ構成について説明してください。',
+			time: 45,
+		},
+		{
+			q: 'テーブル構成の説明をしてください。',
+			time: 45,
+		},
+		{
+			q: 'Webアプリ作りで大変だった点はなんですか？',
+			time: 60,
+		},
+		{
+			q: '実装はどの様に進めましたか？',
+			time: 45,
+		},
+		{
+			q: '追加機能は考えていますか？',
+			time: 45,
+		},
+		{
+			q: 'アプリ/サービスを運用する際にどのようなことに気を付けていますか？',
+			time: 45,
+		},
+	];
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+	const [progress, setProgress] = useState(0);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setProgress((prevProgress) => {
+				if (prevProgress >= 100) {
+					clearInterval(timer);
+					setCurrentQuestionIndex(
+						(prevIndex) => (prevIndex + 1) % questions.length
+					);
+					return 0;
+				}
+				return prevProgress + 100 / questions[currentQuestionIndex].time;
+			});
+		}, 1000);
+		return () => clearInterval(timer);
+	}, [currentQuestionIndex]);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+	const handleNext = () => {
+		setCurrentQuestionIndex((prevIndex) => (prevIndex + 1) % questions.length);
+		setProgress(0);
+	};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	const handleBack = () => {
+		setCurrentQuestionIndex(
+			(prevIndex) => (prevIndex - 1 + questions.length) % questions.length
+		);
+		setProgress(0);
+	};
+
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<div>
+				<Progress value={progress} className="mb-3" />
+				<Card className="w-[350px]">
+					<CardHeader>
+						<CardTitle className="mb-3">
+							{currentQuestionIndex + 1} 問目
+						</CardTitle>
+						<CardTitle>{questions[currentQuestionIndex].q}</CardTitle>
+						<Separator />
+						<CardDescription>
+							{questions[currentQuestionIndex].time}
+							秒で説明できる様にしましょう。
+						</CardDescription>
+					</CardHeader>
+					<CardFooter className="flex justify-between">
+						<Button variant="outline" onClick={handleBack}>
+							戻る
+						</Button>
+						<Button onClick={handleNext}>次へ</Button>
+					</CardFooter>
+				</Card>
+			</div>
+		</div>
+	);
 }
